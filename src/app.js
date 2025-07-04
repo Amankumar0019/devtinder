14e72a6af6486f41337f4f2f212d9dd392ad6e45
Aman Kumar <amankumar9334872128@gmail.com>
2025-07-04 12:47:38 +0530

    dynamic-data

--- src/app.js
+++ src/app.js
@@ -3,14 +3,12 @@ const connectDB=require("./config/database")
 const User=require("./models/user")
 const app = express();
 
+app.use(express.json());
+
+
 app.post("/signup",async(req,res)=>{
 
-  const user= new User({
-    firstName:"Virat",
-    lastName:"Kholi",
-    emailId:"viratkholi@gmail.com",
-    password:"Virat@123"
-  });
+  const user= new User(req.body);
 
   try{
   await user.save();
