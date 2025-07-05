e13dd059b5861fd3ccf68babc109b0e89ccc47b6
Aman Kumar <amankumar9334872128@gmail.com>
2025-07-05 18:04:06 +0530

    Schema-validation

--- src/app.js
+++ src/app.js
@@ -15,10 +15,87 @@ app.post("/signup",async(req,res)=>{
   res.send("User Added successfully");
   }
   catch(err){
-    res.statusMessage(400).send("Error saving the user"+err.message);
+    res.status(400).send("Error saving the user"+err.message);
   }
  
 });
+
+//Get user by email
+
+app.get("/user",async(req,res)=>{
+  const userEmail=req.body.emailId;
+
+  try{
+    const user= await User.find({emailId:userEmail});
+    if(user.length ===0){
+      res.status(404).send("user not found");
+    }
+
+    res.send(user);
+  }
+  catch(err){
+    res.status(400).send("Error fetching the user: " + err.message);
+  }
+})
+
+app.get("/feed",async (req,res)=>{
+try{
+    const users= await User.find({});
+    if(users.length ===0){
+      res.status(404).send("No users found");
+    }
+    res.send(users)
+}
+catch(err){
+    res.status(400).send("Error fetching users: " + err.message);
+  }
+
+
+  
+})
+// delete user by id
+app.delete("/user",async(req,res)=>{
+  const userId=req.body.userId;
+  try{
+    const user=await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully!!");
+  }
+  catch(err){
+    res.status(400).send("Error fetching users: " + err.message);
+  } 
+});
+// Update user by id
+
+app.patch("/user",async(req,res)=>{
+  const userId=req.body.userId;
+  const data=req.body;
+  try{
+    await User.findByIdAndUpdate(userId,data,{
+      runValidators: true
+    });
+    res.send("User updated successfully!!");
+  }
+   catch(err){
+    res.status(400).send("Error fetching users: " + err.message);
+  } 
+})
+
+
+
+// app.patch("/user",async(req,res)=>{
+//   const useremail=req.body.emailId ;
+//   const data=req.body;
+//   try{
+//     await User.findOneAndUpdate({emailId:useremail},data,{
+//       returnDocument: "after",
+//     });
+//     res.send("User updated successfully!!");
+//   }
+//    catch(err){
+//     res.status(400).send("Error fetching users: " + err.message);
+//   } 
+// })
+
 
 connectDB().then(()=>{
     console.log("Database connected successfully");
--- src/models/user.js
+++ src/models/user.js
@@ -2,24 +2,55 @@ const mongoose=require("mongoose");
 
 const userSchema =mongoose.Schema({
     firstName :{
-        type:String
+        type:String,
+        required:true,
+        minlength:4,
+        maxlength:20,
+    
     },
     lastName:{
         type:String
     },
     emailId:{
-        type:String
+        type:String,
+        required:true,
+        unique:true, 
+        lowercase:true,
+        trim:true
     },
     password:{
-        type:String
+        type:String,
+        required:true,
     },
     age:{
-        type:Number
+        type:Number,
+        min:18,
+        max:100,
     },
     gender:{
-        type:String
+        type:String,
+        validate(value){
+            if(!["male","female","others"].includes(value)){
+                throw new Error("Gender data is not correct");
+            }
+        }
+    },
+    photoUrl:{
+        type:String,
+        default:"https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg"
+    },
+    about:{
+        type:String,
+        default:"This is a default about of the user"
+    },
+    skill:{
+        type:[String],
+    },
+},
+{
+        timestamps:true,
     }
 
-});
+);
 
 module.exports=mongoose.model("User",userSchema);
